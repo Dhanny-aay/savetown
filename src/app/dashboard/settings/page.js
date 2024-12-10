bdfe3cdddbd494ec55c93101cdf4641e744d1466
Dhanny-aay <danielakintolu@gmail.com>
2024-12-10 01:36:24 +0100

    update settings

--- src/app/dashboard/settings/page.js
+++ src/app/dashboard/settings/page.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from "react";
 import Personal from "./components/personal";
 import Password from "./components/password";
 import Payment from "./components/payment";
+import forward from "./assets/forward.svg";
+import Cardholder from "./assets/Cardholder.svg";
+import Lock from "./assets/Lock.svg";
+import User from "./assets/User.svg";
+import ArrowSquareOut from "./assets/ArrowSquareOut.svg";
+import ArrowRightBlk from "./assets/ArrowRightBlk.svg";
 
 export default function Page() {
   const [activeButton, setActiveButton] = useState("Personal");
@@ -13,26 +19,31 @@ export default function Page() {
       label: "Personal Info",
       value: "Personal",
       component: <Personal />,
+      icon: User,
     },
     {
       label: "Password",
       value: "Password",
       component: <Password />,
+      icon: Lock,
     },
     {
       label: "Payment Method",
       value: "Payment",
       component: <Payment />,
+      icon: Cardholder,
     },
     {
       label: "Privacy Policy",
       value: "Privacy",
       component: null, // Replace with actual component
+      icon: Lock,
     },
     {
       label: "Log out",
       value: "logout",
       component: null, // Replace with actual component or logout logic
+      icon: ArrowSquareOut,
     },
   ];
 
@@ -56,6 +67,10 @@ export default function Page() {
     setActiveButton(value);
   };
 
+  const goBack = () => {
+    setActiveButton(null);
+  };
+
   // Desktop view with buttons
   const DesktopNavigation = () => (
     <div className="flex mt-6 border-b w-full border-[#EAECF0]">
@@ -74,38 +89,68 @@ export default function Page() {
       ))}
     </div>
   );
-
-  // Mobile view with select dropdown
+  // Mobile Navigation
   const MobileNavigation = () => (
-    <div className="mt-6 px-6 py-3 w-full border border-[#EAECF0] rounded-md">
-      <select
-        value={activeButton}
-        onChange={(e) => handleButtonClick(e.target.value)}
-        className="w-full bg-transparent text-sm font-Manrope"
-      >
+    <div className="mt-6 w-full">
+      <div className=" w-full space-y-3">
         {buttons.map((button, index) => (
-          <option
+          <div
             key={index}
-            value={button.value}
-            className="font-Manrope text-sm"
+            className="w-full p-4 bg-white border rounded-lg border-[#C2C4C6] flex items-center justify-between cursor-pointer"
+            onClick={() => handleButtonClick(button.value)}
           >
-            {button.label}
-          </option>
+            <div className=" flex space-x-4 items-center">
+              <span className=" w-12 h-12 rounded-full bg-[#F2F3F4] flex items-center justify-center">
+                <img src={button.icon.src} alt="" />
+              </span>
+              <span className="text-[#262626] font-Manrope text-sm font-medium">
+                {button.label}
+              </span>
+            </div>
+            <img src={forward.src} className=" h-3" alt="" />
+          </div>
         ))}
-      </select>
+      </div>
     </div>
   );
+
+  // Display Active Component for Mobile
+  const ActiveComponentMobile = () => {
+    const activeButtonData = buttons.find(
+      (button) => button.value === activeButton
+    );
+    return (
+      <div className="w-full">
+        <button
+          onClick={goBack}
+          className="text-btnPrimary font-Manrope text-sm mb-6 flex items-center mt-4"
+        >
+          <img src={ArrowRightBlk.src} className=" w-4" alt="" />
+        </button>
+        {activeButtonData?.component || null}
+      </div>
+    );
+  };
 
   return (
     <>
       <p className="text-[#262626] block md:hidden font-bold text-lg py-3 md:text-xl leading-none font-Manrope">
         Settings
       </p>
-      {isMobile ? <MobileNavigation /> : <DesktopNavigation />}
+      {isMobile ? (
+        activeButton ? (
+          <ActiveComponentMobile />
+        ) : (
+          <MobileNavigation />
+        )
+      ) : (
+        <DesktopNavigation />
+      )}
 
       <div className="mt-6">
-        {buttons.find((button) => button.value === activeButton)?.component ||
-          null}
+        {!isMobile &&
+          (buttons.find((button) => button.value === activeButton)?.component ||
+            null)}
       </div>
     </>
   );
